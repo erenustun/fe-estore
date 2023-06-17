f2ec5517f99273d4fdaa182f539163f6b95daacc
erenustun <eren.uestuen@gmail.com>
2023-06-17 05:01:12 +0200

    feat: badge component

---
+++ app/_components/Badge.tsx
@@ -0,0 +1,19 @@
+import tw from 'tailwind-styled-components'
+
+export const Badge = tw.span`
+  absolute
+  top-0
+  right-0
+  -mt-2
+  -mr-2
+  bg-rose-500
+  text-slate-50
+  font-semibold
+  h-4
+  w-4
+  text-xs
+  rounded-full
+  flex
+  items-center
+  justify-center
+`
