28f27031d7cfb08555dd46b57239cdb70fe0e91e
erenustun <eren.uestuen@gmail.com>
2023-06-19 04:45:49 +0200

    feat: next migration to pages routing

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Products() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-6xl items-center justify-between lg:flex">
