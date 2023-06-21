186db60ea8b3ba58f0bb49f9860900b5d7467d8c
erenustun <eren.uestuen@gmail.com>
2023-06-21 23:44:52 +0200

    refactor: get bearer token hook

--- src/util/api.util.ts
+++ src/util/api.util.ts
@@ -1,8 +1,7 @@
 export const getBearerToken = () => {
-  if (typeof localStorage !== 'undefined') {
-    const token = JSON.parse(localStorage.getItem('auth-storage') as string)
-      ?.state?.accessToken
-    return token ? token : null
-  }
+  if (typeof localStorage !== 'undefined') console.log(document.cookie)
+  const jwt =
+    typeof localStorage !== 'undefined' && document.cookie.split('=')[1]
+  if (typeof jwt !== 'undefined') return jwt
   return null
 }
