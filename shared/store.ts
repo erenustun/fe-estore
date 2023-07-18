2db46bba20192e43ea4b54163162ba18a0672f41
erue <eren.uestuen@gmail.com>
2023-07-18 02:08:18 +0200

    refactor: zustand store

--- shared/store.ts
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-import { createAuthStore, IAuthStore } from '@feature/auth/store/auth.store'
-
-export const useZustand = create<IAuthStore>()((...a) => ({
-  ...createAuthStore(...a)
-}))
