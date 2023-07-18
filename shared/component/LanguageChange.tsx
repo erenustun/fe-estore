778bfc7d3272e395e91ecb67aa4d4ce5354d232d
erue <eren.uestuen@gmail.com>
2023-07-18 02:38:52 +0200

    refactor: LanguageChange.tsx

--- shared/component/LanguageChange.tsx
+++ shared/component/LanguageChange.tsx
@@ -1,33 +1,31 @@
-import { useCookies } from 'react-cookie'
 import { LanguageIcon } from '@heroicons/react/24/solid'
 import cn from 'classnames'
 import { useRouter } from 'next/router'
+import useUserStore from '@feature/account/state/user.store'
 
 interface LanguageChangeProps {
   className?: string
 }
 
 export const LanguageChange = ({ className }: LanguageChangeProps) => {
-  const { locale, locales, defaultLocale } = useRouter()
+  const router = useRouter()
+  const { pathname, asPath, query, push } = router
 
-  const [cookies, setCookie, removeCookie] = useCookies(['locale'])
+  const { settings, setLanguage } = useUserStore(state => state)
 
-  /*  console.log('locale:')
-  console.log(cookies['locale'])
-
-  useEffect(() => {
-    console.log(locale)
-    console.log(locales)
-    console.log(defaultLocale)
-    /!*if (cookies['locale'])
-      console.log(cookies['locale'])*!/
-  })*/
+  const handleChange = (item: { intl: string }) => {
+    setLanguage(item.intl)
+    push({ pathname, query }, asPath, { locale: item.intl })
+  }
 
   return (
     <div
-      className={cn('flex items-center space-x-1 cursor-pointer', className)}
+      className={cn(
+        'flex items-center space-x-1 cursor-pointer uppercase',
+        className
+      )}
     >
-      <span>EN</span>
+      <span>{settings?.language}</span>
       <LanguageIcon className="h-4 w-4" />
     </div>
   )
