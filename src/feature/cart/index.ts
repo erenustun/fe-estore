4cd586d2f2d934bf7c32db90d96df63fd771ca1d
erue <eren.uestuen@gmail.com>
2023-10-11 04:48:05 +0200

    hotfix: left-over upcoming feature

--- src/feature/cart/index.ts
+++ src/feature/cart/index.ts
@@ -1,6 +1,3 @@
-export * from './component/shipping.component'
-export * from './component/shipping-select.component'
-export * from './component/payment.component'
 export * from './component/cart-empty.component'
 export * from './component/cart-modal.component'
 export * from './component/view-cart.component'
@@ -8,4 +5,3 @@ export * from './component/view-cart.component'
 export * from './state/cart.dto'
 
 export * from './view/cart.view'
-export * from './view/checkout.view'
