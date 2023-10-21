b79934187afbcb1433a403bd7655694080b3b75d
erue <eren.uestuen@gmail.com>
2023-10-21 03:51:13 +0200

    refactor: page head for document title

--- pages/account/address/index.tsx
+++ pages/account/address/index.tsx
@@ -1,4 +1,23 @@
 import { AccountView } from '@feature/account'
+import { useIntl } from 'react-intl'
+import Head from 'next/head'
 
-const AccountAddress = () => <AccountView />
+const AccountAddress = () => {
+  const intl = useIntl()
+
+  return (
+    <>
+      <Head>
+        <title>
+          {intl.formatMessage({ id: 'header_navigation_user_account' })} -{' '}
+          {intl.formatMessage({ id: 'app_slogan' })} -
+          {intl.formatMessage({ id: 'app_domain' })}
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AccountView />
+    </>
+  )
+}
+
 export default AccountAddress
--- pages/cart/checkout/index.tsx
+++ pages/cart/checkout/index.tsx
@@ -1,23 +1,23 @@
-import { HomeView } from '@feature/home'
-import Head from 'next/head'
 import { useIntl } from 'react-intl'
+import Head from 'next/head'
+import { CheckoutView } from '@feature/cart'
 
-const Home = () => {
+const CartPage = () => {
   const intl = useIntl()
 
   return (
     <>
       <Head>
         <title>
-          {intl.formatMessage({ id: 'header_navigation_home' })} -{' '}
-          {intl.formatMessage({ id: 'app_slogan' })} -{' '}
+          {intl.formatMessage({ id: 'navigation_checkout' })} -{' '}
+          {intl.formatMessage({ id: 'app_slogan' })} -
           {intl.formatMessage({ id: 'app_domain' })}
         </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <HomeView />
+      <CheckoutView />
     </>
   )
 }
 
-export default Home
+export default CartPage
